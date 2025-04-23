2118eaec581c638c821eb927dd44991d7ee57679
DelaMaia <106710526+cpMaia@users.noreply.github.com>
2025-04-23 11:38:37 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    contador += 1
+    basic.showNumber(contador)
+})
+input.onButtonPressed(Button.B, function () {
+    contador += -1
+    basic.showNumber(contador)
+})
+let contador = 0
+contador = 0
